98d77185751db94f20de22fe1a8ef653a7e4666e
shinseongsu <tlstjdtn321@naver.com>
2025-04-04 03:52:25 +0900

    feat: 계층간 mapper 클래스 정의

---
+++ user/src/main/kotlin/com/noonoo/application/event/mapper/EmailSendEventMapper.kt
@@ -0,0 +1,36 @@
+package com.noonoo.application.event.mapper
+
+import com.noonoo.domain.collection.EmailSendEvent
+import com.noonoo.domain.collection.EmailSendEventCollection
+import com.noonoo.domain.model.Members
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.springframework.stereotype.Component
+
+@Component
+class EmailSendEventMapper {
+
+    fun mapper(members: Members) : EmailSendEvent {
+        return EmailSendEvent(
+            memberId = members.id!!,
+            email = members.email,
+            name = members.name,
+            eventType = "SIGN_UP",
+            attributes = "회원가입 축하드립니다.",
+        )
+    }
+
+    fun mapper(emailSendEvent: EmailSendEvent) : EmailSendEventCollection {
+        return EmailSendEventCollection(
+            memberId = emailSendEvent.memberId,
+            name = emailSendEvent.name,
+            email = emailSendEvent.email,
+            eventType = emailSendEvent.eventType,
+            attributes = emailSendEvent.attributes,
+            isSendMail = false,
+            createAt = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()),
+        )
+    }
+
+}
