51e427c51e0aa15d5012d4e8cfd7bd11f35fa5b8
shinseongsu <tlstjdtn321@naver.com>
2025-04-04 03:46:37 +0900

    init: 초기 프로젝트 설정

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "noonoo"
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include(
+    "user"
+)
