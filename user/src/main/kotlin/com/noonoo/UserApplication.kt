ec44b29fca3afb508b06ec72c1d0fa5229c4c99b
shinseongsu <tlstjdtn321@naver.com>
2025-04-04 03:49:57 +0900

    feat: 스프링 application

---
+++ user/src/main/kotlin/com/noonoo/UserApplication.kt
@@ -0,0 +1,13 @@
+package com.noonoo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+class UserApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserApplication>(*args)
+}
