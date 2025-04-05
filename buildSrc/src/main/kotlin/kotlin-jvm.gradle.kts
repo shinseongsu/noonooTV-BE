a476c9d65bdd37866c9271500bf09db1ea1c129e
shinseongsu <tlstjdtn321@naver.com>
2025-04-05 23:51:39 +0900

    chore: 라인 관리 수정

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -15,30 +15,4 @@ kotlin {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
-
-    testLogging {
-        events(
-            TestLogEvent.FAILED,
-            TestLogEvent.PASSED,
-            TestLogEvent.SKIPPED
-        )
-    }
-}
-
-if (!rootProject.extra.has("buildsrc.internal.install-git-hooks")) {
-    rootProject.extra.set("buildsrc.internal.install-git-hooks", true)
-
-    val preCommit by project.rootProject.tasks.creating(InstallPreCommitHookTask::class) {
-        group = "build setup"
-        description = "Installs Kotlinter Git pre-commit hook"
-    }
-
-    val prePush by project.rootProject.tasks.creating(InstallPrePushHookTask::class) {
-        group = "build setup"
-        description = "Installs Kotlinter Git pre-push hook"
-    }
-
-    project.rootProject.tasks.getByName("prepareKotlinBuildScriptModel") {
-        dependsOn(preCommit, prePush)
-    }
 }
